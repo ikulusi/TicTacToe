218deaf65711b65df73c007f6d62f4973362a1cc
ivan kulušić <ivan.kulusic@hotmail.com>
2015-12-14 13:41:45 +0100

    Added tests project

---
+++ ticTacToe.Tests/PlayerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using ticTacToe.Logic;
+
+namespace ticTacToe.Tests
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void NewPlayerIsAlwaysFirstPlayer()
+        {
+            var player = new Player();
+
+            Assert.AreEqual(PlayerType.First, player.Who());
+        }
+    }
+}
