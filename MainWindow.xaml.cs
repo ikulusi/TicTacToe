ee9649f758c55d7d75cb200f3e82d02f14186f6f
ivan kulušić <ivan.kulusic@hotmail.com>
2015-12-14 12:59:48 +0100

    Fixed logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,32 +9,7 @@ namespace ticTacToe
     
 
 
-    public class Player
-    {
-        int player;
-
-        public Player()
-        {
-            player = 0;
-        }
-
-        public void Reset()
-        {
-            player = 0;
-        }
-
-        public void ChangePlayer()
-        {
-            player += 1;
-            player %= 2;
-        }
-
-        public string Who()
-        {
-            if (player == 0) return "o";
-            else return "x";
-        }
-    }
+    
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ ticTacToe.Logic/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticTacToe.Logic
+{
+    public class Player
+    {
+        int player;
+
+        public Player()
+        {
+            player = 0;
+        }
+
+        public void Reset()
+        {
+            player = 0;
+        }
+
+        public void ChangePlayer()
+        {
+            player += 1;
+            player %= 2;
+        }
+
+        public string Who()
+        {
+            if (player == 0) return "o";
+            else return "x";
+        }
+    }
+}
