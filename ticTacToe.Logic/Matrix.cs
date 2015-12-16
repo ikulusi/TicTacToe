e810c87900e85a958afe1a6b9262632add9f6a51
ivan kulušić <ivan.kulusic@hotmail.com>
2015-12-16 14:51:00 +0100

    Coloring winning path

--- ticTacToe.Logic/Matrix.cs
+++ ticTacToe.Logic/Matrix.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ticTacToe.Logic
 {
     public class Matrix
     {
         int[,] matrix = new int[3, 3];
+        int[] path = new int[12];
+        int pathCounter;
+       
 
         public Matrix()
         {
@@ -17,7 +21,13 @@ namespace ticTacToe.Logic
 
         public bool IsRowFull(int i)
         {
-            if (matrix[i, 0] == matrix[i, 1] && matrix[i, 0] == matrix[i, 2] && matrix[i, 0] != 0) return true;
+            if (matrix[i, 0] == matrix[i, 1] && matrix[i, 0] == matrix[i, 2] && matrix[i, 0] != 0)
+            {
+                path[pathCounter++] = i; path[pathCounter++] = 0;
+                path[pathCounter++] = i; path[pathCounter++] = 1;
+                path[pathCounter++] = i; path[pathCounter++] = 2;
+                return true;
+            }
             else return false;
         }
 
@@ -26,27 +36,63 @@ namespace ticTacToe.Logic
             for (int i = 0; i < 3; ++i)
                 for (int j = 0; j < 3; ++j)
                     matrix[i, j] = 0;
+            for (int i = 0; i < 12; ++i)
+                path[i] = -1;
+            pathCounter = 0;
         }
 
         public bool IsColumnFull(int i)
         {
-            if (matrix[0, i] == matrix[1, i] && matrix[0, i] == matrix[2, i] && matrix[0, i] != 0) return true;
+            if (matrix[0, i] == matrix[1, i] && matrix[0, i] == matrix[2, i] && matrix[0, i] != 0)
+            {
+                path[pathCounter++] = 0; path[pathCounter++] = i;
+                path[pathCounter++] = 1; path[pathCounter++] = i;
+                path[pathCounter++] = 2; path[pathCounter++] = i;
+                return true;
+            }
             else return false;
         }
 
         public bool IsDiagonalFull()
         {
-            if ((matrix[0, 0] == matrix[1, 1] && matrix[1, 1] == matrix[2, 2] && matrix[0, 0] != 0) ||
-                (matrix[0, 2] == matrix[1, 1] && matrix[1, 1] == matrix[2, 0] && matrix[1, 1] != 0))
-
+            if ((matrix[0, 0] == matrix[1, 1] && matrix[1, 1] == matrix[2, 2] && matrix[0, 0] != 0) )
+            {
+                path[pathCounter++] = 0; path[pathCounter++] = 0;
+                path[pathCounter++] = 1; path[pathCounter++] = 1;
+                path[pathCounter++] = 2; path[pathCounter++] = 2;
                 return true;
+            }
+
+            else return false;
+        }
+
+        public bool IsReversedDiagonalFull()
+        {
+            if ((matrix[0, 2] == matrix[1, 1] && matrix[1, 1] == matrix[2, 0] && matrix[1, 1] != 0))
+            {
+                path[pathCounter++] = 0; path[pathCounter++] = 2;
+                path[pathCounter++] = 1; path[pathCounter++] = 1;
+                path[pathCounter++] = 2; path[pathCounter++] = 0;
+                return true;
+            }
             else return false;
         }
 
         public bool IsWin(int i, int j)
         {
-            if (IsColumnFull(j) || IsRowFull(i) || IsDiagonalFull()) return true;
-            else return false;
+            bool win = false;
+            /* 
+            pozivam za svaku funkciju posebno jer se ovako zaustavi izvrsavanje cim je jedna f-ja prosla, a trebaju se sve
+            izvrsit da bi se ispunio path
+                if (IsColumnFull(j) || IsRowFull(i) || IsDiagonalFull() || IsReversedDiagonalFull() ) return true;
+            */
+            if (IsColumnFull(j)) win = true;
+            if (IsRowFull(i)) win = true;
+            if (IsDiagonalFull()) win = true;
+            if (IsReversedDiagonalFull()) win = true;
+
+
+            return win;
         }
 
         public int element(int i, int j)
@@ -67,5 +113,12 @@ namespace ticTacToe.Logic
                     if (IsWin(i, j)) return true;
             return false;
         }
+
+        public int[] WiningPath()
+        {
+            return path;
+        } 
+
+              
     }
 }
